dee1d302d9bef717c3339889b55e6615058b5339
CodeNinjasDripping <161961323+CodeNinjasDripping@users.noreply.github.com>
2025-03-15 15:31:32 -0500

    Step 26

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    public float jumpPower = 6f;
+    public bool isGrounded;
+    float posX =0.0f;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        posX = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.GetKeyDown(KeyCode.Space)&&isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpPower * rb.mass * rb.gravityScale *20f);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collisoin)
+    {
+        if(collisoin.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+            
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
     }
 }
+
